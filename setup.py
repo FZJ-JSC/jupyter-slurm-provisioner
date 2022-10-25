ced603cfa4fdb2837e22a612bff9707b2b9fd417
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2022-10-25 11:37:25 +0200

    add long_description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+try:
+    with open("README") as f:
+        long_description = f.read()
+except Exception:
+    long_description = ""
+
 setup(
     name="slurm_provisioner",
-    version="0.1.1",
+    version="0.1.2",
     description="Jupyter slurm kernel provisioner",
     url="https://github.com/FZJ-JSC/jupyter-slurm-provisioner",
     author="Tim Kreuzer",
@@ -10,6 +16,7 @@ setup(
     license="MIT",
     packages=["slurm_provisioner"],
     install_requires=["jupyter_client>=7.1.2"],
+    long_description=long_description,
     entry_points={
         "jupyter_client.kernel_provisioners": [
             "slurm-provisioner = slurm_provisioner:SlurmProvisioner",
