76f7852abafa4f68c812d8c81c7efc165687f8c8
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2022-11-11 09:17:30 +0100

    version 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except Exception:
 
 setup(
     name="jupyter-slurm-provisioner",
-    version="0.1.10",
+    version="0.2.0",
     description="Jupyter slurm kernel provisioner",
     url="https://github.com/FZJ-JSC/jupyter-slurm-provisioner",
     author="Tim Kreuzer",
